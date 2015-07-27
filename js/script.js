f9546fa7306112359baf94fa05b75cb6705a373e
Oliver Cole <hive7@Olivers-MacBook-Air.local>
2015-07-27 14:58:40 +0100

    Started body

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 window.onload = function () {
-	dropdownBars = document.getElementById('bars');
-	navItems = document.getElementById('nav-items');
+	var dropdownBars = document.getElementById("bars"),
+		navItems = document.getElementById("nav-items"),
+		as = navItems.getElementsByTagName('a');
+	for (var i = as.length - 1; i >= 0; i--) {
+		as[i].onfocus = function () {
+			this.blur();
+		}
+	};
 	dropdownBars.onclick = function () {
-		if (navItems.className == 'toggled') {
-			navItems.className = '';
+		if (navItems.className == "toggled") {
+			navItems.className = "";
 		} else {
-			navItems.className = 'toggled';
+			navItems.className = "toggled";
 		};
 	}
 }
