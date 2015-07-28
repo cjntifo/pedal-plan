ca3ffac2d6e8cd2283da377f3f7a0e77517dde45
Alexander Nielsen <hitecherik@gmail.com>
2015-07-28 12:04:59 +0100

    Added UK Postcodes integration and formatted Google's html instructions.

--- results.php
+++ results.php
@@ -3,11 +3,23 @@
 	
 	$start = urlencode($_GET["start"]);
 	$end = urlencode($_GET["end"]);
-	$mode = urlencode($_GET["mode"]);
-	$url = "https://maps.googleapis.com/maps/api/directions/json?origin=$start&destination=$end&key=$key&alternatives=true&mode=$mode";
+	$url = "https://maps.googleapis.com/maps/api/directions/json?origin=$start&destination=$end&key=$key&alternatives=true&mode=bicycling";
 	
 	$route = json_decode(file_get_contents($url));
+	$postcodes = [];
+	$instructions = [];
 	
 	foreach ($route->routes[0]->legs[0]->steps as $step) {
-		echo $step->html_instructions . "<br>";
+		$lat = urlencode(($step->start_location->lat + $step->end_location->lat) / 2);
+		$lng = urlencode(($step->start_location->lng + $step->end_location->lng) / 2);
+		$api_call = simplexml_load_file("http://uk-postcodes.com/postcode/nearest?lat=$lat&lng=$lng&miles=0.05&format=xml");
+		
+		$instruction = strip_tags(str_replace("<div", ". <div", $step->html_instructions), "<b>") . ".";
+		
+		foreach ($api_call->postcodes->postcode as $postcode) {
+			array_push($postcodes, $postcode->postcode);
+		}
+		
+		array_push($instructions, $instruction);
+		echo "<p>$instruction</p>";
 	}
