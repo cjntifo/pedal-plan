ec9c789fcb1f68b17318f22b45590c4f97010509
Oliver Cole <hive7@Olivers-MacBook-Air.local>
2015-07-28 12:32:29 +0100

    IE bug testing

---
+++ js/iescript.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+	var inputs = document.getElementsByTagName('input');
+	for (var i = inputs.length - 1; i >= 0; i--) {
+		inputs[i].value = inputs[i].getAttribute('placeholder');
+		inputs[i].onfocus = function () {
+			this.value = '';
+		}
+		inputs[i].onblur = function() {
+			if (this.value == '') {
+				this.value = this.getAttribute('placeholder');
+			}
+		}
+	}
+};
--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
 window.onload = function () {
-	var ie = (function(){
-	    var undef,
-	        v = 3,
-	        div = document.createElement('div'),
-	        all = div.getElementsByTagName('i');
-	    while (div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->', all[0]);
-	    return v > 4 ? v : undef;
-	}());
 	var dropdownBars = document.getElementById("bars"),
 		navItems = document.getElementById("nav-items"),
-		as = navItems.getElementsByTagName('a'),
-		inputs = document.getElementsByTagName('input');
+		as = navItems.getElementsByTagName('a');
 	for (var i = as.length - 1; i >= 0; i--) {
 		as[i].onfocus = function () {
 			this.blur();
@@ -24,16 +15,6 @@ window.onload = function () {
 		}
 	}
 	if (ie < 10) {
-		for (var i = inputs.length - 1; i >= 0; i--) {
-			inputs[i].value = inputs[i].getAttribute('placeholder');
-			inputs[i].onfocus = function () {
-				this.value = '';
-			}
-			inputs[i].onblur = function() {
-				if (this.value == '') {
-					this.value = this.getAttribute('placeholder');
-				}
-			}
-		}
+		
 	}
 }
