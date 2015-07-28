17ed30561396406e77df621bfe10a5167d2546ab
Oliver Cole <hive7@Olivers-MacBook-Air.local>
2015-07-28 12:12:34 +0100

    Support for ie placeholders added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 window.onload = function () {
+	var ie = (function(){
+	    var undef,
+	        v = 3,
+	        div = document.createElement('div'),
+	        all = div.getElementsByTagName('i'),
+	        inputs = document.getElementsByTagName('input');
+	    while (div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->', all[0]);
+	    return v > 4 ? v : undef;
+	}());
 	var dropdownBars = document.getElementById("bars"),
 		navItems = document.getElementById("nav-items"),
 		as = navItems.getElementsByTagName('a');
@@ -6,12 +15,25 @@ window.onload = function () {
 		as[i].onfocus = function () {
 			this.blur();
 		}
-	};
+	}
 	dropdownBars.onclick = function () {
 		if (navItems.className == "toggled") {
 			navItems.className = "";
 		} else {
 			navItems.className = "toggled";
-		};
+		}
+	}
+	if (ie < 10) {
+		for (var i = inputs.length - 1; i >= 0; i--) {
+			inputs[i].value = inputs[i].placeholder;
+			inputs[i].onfocus = function () {
+				inputs[i].value = '';
+			}
+			inputs[i].onblur = function() {
+				if (inputs[i].value == '') {
+					inputs[i].value = inputs[i].placeholder;
+				}
+			}
+		}
 	}
 }
